7337e73ec33a237281a7b314728e95770ddac931
enesyusufgokce <141220587+enesyusufgokce@users.noreply.github.com>
2025-08-02 21:10:28 +0300

    extended features

--- VideoLikesPrediction.py
+++ VideoLikesPrediction.py
@@ -1,18 +1,24 @@
 from sklearn.linear_model import LinearRegression
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler, PolynomialFeatures
 import pandas as pd
 
-data = pd.read_csv("likes_training_data.csv")
+data = pd.read_csv("likes_training_data_realistic.csv")
 model = LinearRegression()
 
-x = data[["views", "video_age"]]
+x = data[["views", "video_age", "video_duration"]]
 y = data[["likes"]]
 
+polynomial_funct = PolynomialFeatures(degree=3)
+polynomial_x = polynomial_funct.fit_transform(x)
+
 scaler = StandardScaler()
-scaled_x = scaler.fit_transform(x)    # data were normalized by using z-score normalization
+scaled_x = scaler.fit_transform(polynomial_x)    # data were normalized by using z-score normalization
+
 model.fit(scaled_x,y)
-prediction_init = model.predict(scaler.transform([
-                                                 [33690,5]
-                                                 ]))
+
+prediction_data = [
+                  [30022,8,6.16]
+                  ]
+prediction_init = model.predict(scaler.transform(polynomial_funct.fit_transform(prediction_data)))
 prediction = int(prediction_init[0][0])
-print("Estimated number of likes for a video viewed 33690 times and uploaded 5 years ago is:", prediction)
+print("Estimated number of likes for a video viewed {} times, uploaded {} years ago and duration of {} minutes is:".format(prediction_data[0][0],prediction_data[0][1],prediction_data[0][2]), prediction)
