8499ec9205228311c6cc8621f82b6e0bc08bc139
enesyusufgokce <141220587+enesyusufgokce@users.noreply.github.com>
2025-08-02 21:27:46 +0300

    extended features

--- VideoLikesPrediction.py
+++ VideoLikesPrediction.py
@@ -17,8 +17,15 @@ scaled_x = scaler.fit_transform(polynomial_x)    # data were normalized by using
 model.fit(scaled_x,y)
 
 prediction_data = [
-                  [30022,8,6.16]
+                  [30022, 8, 23],
+                  [300, 1, 12],
+                  [13435, 6, 2.26]
                   ]
 prediction_init = model.predict(scaler.transform(polynomial_funct.fit_transform(prediction_data)))
-prediction = int(prediction_init[0][0])
-print("Estimated number of likes for a video viewed {} times, uploaded {} years ago and duration of {} minutes is:".format(prediction_data[0][0],prediction_data[0][1],prediction_data[0][2]), prediction)
+prediction1 = int(prediction_init[0][0])
+prediction2 = int(prediction_init[1][0])
+prediction3 = int(prediction_init[2][0])
+
+print("Estimated number of likes for a video viewed {} times, uploaded {} years ago and duration of {} minutes is:".format(prediction_data[0][0],prediction_data[0][1],prediction_data[0][2]), prediction1)
+print("Estimated number of likes for a video viewed {} times, uploaded {} years ago and duration of {} minutes is:".format(prediction_data[1][0],prediction_data[1][1],prediction_data[1][2]), prediction2)
+print("Estimated number of likes for a video viewed {} times, uploaded {} years ago and duration of {} minutes is:".format(prediction_data[2][0],prediction_data[2][1],prediction_data[2][2]), prediction3)
