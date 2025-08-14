f4dd8c1ea375c690c63a41e2ef8f721c90a0a909
enesyusufgokce <141220587+enesyusufgokce@users.noreply.github.com>
2025-08-14 16:35:43 +0300

    fixed code
    At the initial stage of transforming the prediction data into a polynomial function, the process was initially both fit and transformed, but this was corrected to only apply the transform, ensuring that our model learned correctly from the training dataset.

--- VideoLikesPrediction.py
+++ VideoLikesPrediction.py
@@ -1,3 +1,4 @@
+import numpy as np
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import StandardScaler, PolynomialFeatures
 import pandas as pd
@@ -6,7 +7,7 @@ data = pd.read_csv("likes_training_data_realistic.csv")
 model = LinearRegression()
 
 x = data[["views", "video_age(years)", "video_duration(minutes)"]]
-y = data[["likes"]]
+y = data["likes"]
 
 polynomial_funct = PolynomialFeatures(degree=3)
 polynomial_x = polynomial_funct.fit_transform(x)      # features are extended to third degree polynomial terms
@@ -16,10 +17,10 @@ scaled_x = scaler.fit_transform(polynomial_x)    # data were normalized by using
 
 model.fit(scaled_x,y)
 
-prediction_data = [
-                  [30622, 8, 21.3]
-                  ]
-prediction_init = model.predict(scaler.transform(polynomial_funct.fit_transform(prediction_data)))
+prediction_data = np.array([
+                           [30639, 5, 6.22]
+                           ])
+prediction_init = model.predict(scaler.transform(polynomial_funct.transform(prediction_data)))
 
-prediction = int(prediction_init[0][0])
+prediction = int(prediction_init[0])
 print("Estimated number of likes for a video viewed {} times, uploaded {} years ago and duration of {} minutes is:".format(prediction_data[0][0],prediction_data[0][1],prediction_data[0][2]), prediction)
